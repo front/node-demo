4937e99f9641c3e3d51543b0073112e60929fbd8
Henrik Akselsen <henrik.akselsen@gmail.com>
2012-11-09 11:01:54 +0100

    Added strategy.jade, removed user.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -28,10 +27,9 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/strategies', routes.strategies);
 app.post('/', routes.post);
-app.get('/strategy/:title',routes.strategy);
+app.get('/strategies', routes.strategies);
+app.get('/strategy/:title', routes.strategy);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
