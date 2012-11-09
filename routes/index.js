e329d2972f056b24b44955d631749706e33a55cf
Henrik Akselsen <henrik.akselsen@gmail.com>
2012-11-09 09:51:47 +0100

    Setup routes for posts, homepage and strategies

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,42 @@
+// Set up db and models
+var mongoose    = require('mongoose'),
+strategyModel = require('../models/StrategyModel');
 
-/*
- * GET home page.
- */
+// Connect to DB
+mongoose.connect('mongodb://localhost/members');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+
+
+ exports.index = function(req, res){
+ 	res.render('index', { title: 'Express' });
+ };
+
+ exports.post = function(req,res){
+ 	strategy = new strategyModel();
+ 	strategy.title = req.body.title;
+ 	strategy.vision = req.body.vision;
+ 	console.log(strategy);
+
+ 	strategy.save(function (err){
+ 		if(err){
+ 			console.log(err);
+ 		}
+ 		else {
+ 			console.log( "saved strategy");
+ 		}
+ 	});
+
+ 	res.render('index.jade', { title: 'My Registration App'});
+ }
+
+
+ exports.strategies = function(req,res){
+ 	console.log('strategies');
+ 	strategyModel.find({}, function(err,searchResult){
+ 		if(err){
+ 			console.log(err);
+ 		}
+ 		res.render('strategies.jade', {title: 'Strategies', strategies: searchResult})
+ 	});
+ }
+
